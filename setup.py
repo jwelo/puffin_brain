3dd63a8e427cd93d468f5caac9c4572a203f0e06
Joel <joelong345@gmail.com>
2025-06-26 17:49:18 +0800

    fixed build bugs, mainly wrong name

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'agent-turtle'
+package_name = 'puffin_brain'
 
 setup(
     name=package_name,
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yeml]'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,9 +23,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'command_publisher_node = agent-turtle.command_publisher_node:main',
-            'ollama_publisher_node = agent-turtle.ollama_publisher_node:main',
-            'whisper_listener_node = agent-turtle.whisper_listener_node:main',
+            'command_publisher_node = puffin_brain.command_publisher_node:main',
+            'ollama_publisher_node = puffin_brain.ollama_publisher_node:main',
+            'whisper_listener_node = puffin_brain.whisper_listener_node:main',
         ],
     },
 )
